b3efe69491b23bc563828474e76e2548d76965db
Battelman2 <Battelman2@gmail.com>
2014-02-20 08:40:21 -0600

    Added CommandExecutor. Added version and build numbers, may need to remove later if can be retrieved from plugin.yml. Created command "/sm" to return StackMarket status.

--- StackMarket/src/io/github/Battelman2/StackMarket/StackMarket.java
+++ StackMarket/src/io/github/Battelman2/StackMarket/StackMarket.java
@@ -4,5 +4,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class StackMarket extends JavaPlugin
 {
-
+	
+	private static String version;
+	
+	// Increment build every time StackMarket is exported to jar. Build # is independednt from version.
+	private static String build;
+	
+	public void onLoad()
+	{
+		
+	}
+	
+	public void onEnable()
+	{
+		version = "1.0.0";
+		build = "1";
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+	
+	public static String getVersion()
+	{
+		return version;
+	}
+	
+	public static String getBuild()
+	{
+		return build;
+	}
+	
 }
---
+++ StackMarket/src/io/github/Battelman2/StackMarket/StackMarketCommandExecutor.java
@@ -0,0 +1,32 @@
+package io.github.Battelman2.StackMarket;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StackMarketCommandExecutor implements CommandExecutor
+{
+
+	public StackMarketCommandExecutor()
+	{
+		
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if(cmd.getName().equalsIgnoreCase("sm"))
+		{
+			if(args.length == 0)
+			{
+				sender.sendMessage(ChatColor.GRAY + "StackMarket is currently " + ChatColor.GREEN + "ENABLED" + ChatColor.GRAY + " with version " + ChatColor.AQUA + StackMarket.getVersion());
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
+
+}
